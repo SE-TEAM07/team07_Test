00ce25792eafbe1a4e73883bb1e0afc2d738b658
JohnR46 <Machine.The@hotmail.com>
2019-01-27 17:43:59 +0700

    แก้drugdatacontroller ให้สามารถ get medicine ขึ้นไป หาdrug - data ได้

--- frontend/Team07/src/app/redcord-use/redcord-use.component.ts
+++ frontend/Team07/src/app/redcord-use/redcord-use.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CategoryService } from '../Service/category.service';
 import { Router } from "@angular/router";
-import {ActivatedRoute} from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
+
+// import service 
+import { RegisterService } from '../Service/register.service';  // get initial gender bloodtype all
+import { CategoryService } from '../Service/category.service'; // get category all
+import { RedcorduseService } from '../Service/redcorduse.service';  // get Customer by Id
+
 
 @Component({
   selector: 'app-redcord-use',
@@ -10,23 +15,44 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./redcord-use.component.css']
 })
 export class RedcordUseComponent implements OnInit {
-  category : Array<any>;
-  categoryselect ='';
+  // get data from db  
+  category: Array<any>;
+  categoryselect = '';
+  Iinitials: Array<any>;
+  Initialselect = '';
+  genders: Array<any>;
+  genderselect = '';
+  bloodtypes: Array<any>;
+  bloodtypeselect = '';
+  customer: Array<any>;
+  customerselect = '';
 
-  data :any ={}
-  datas :any ={}
+  data: any = {}
+  datas: any = {}
 
-  constructor(private route:ActivatedRoute,private router:Router,private categoryService: CategoryService, private httpClient: HttpClient) { }
+  constructor(private route: ActivatedRoute, private redcorduseservice: RedcorduseService, private registerservice: RegisterService, private router: Router, private categoryservice: CategoryService, private httpClient: HttpClient) { }
 
 
   ngOnInit() {
-    this.route.params.subscribe(prams=>{
+    this.route.params.subscribe(prams => {
       this.datas = prams
       console.log(prams)
     })
-    this.categoryService.getCategory().subscribe(data=>{
+    this.categoryservice.getCategory().subscribe(data => {
       this.category = data;
       console.log(this.category)
+    })
+    this.registerservice.getInitials().subscribe(data => {
+      this.Iinitials = data;
+      console.log(this.Iinitials)
+    })
+    this.registerservice.getGenders().subscribe(data => {
+      this.bloodtypes = data;
+      console.log(this.bloodtypes)
+    })
+    this.redcorduseservice.getCustomerById(Number(1)).subscribe(data => {
+      this.customer = data;
+      console.log(this.customer)
     })
   }
 
