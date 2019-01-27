22ee1c5d7d3a6ea45558bf7fb2b5c23717014af6
JohnR46 <Machine.The@hotmail.com>
2019-01-28 02:44:51 +0700

    close #12 ทำการเก็บข้อมูลลง DB พร้อมปรับ UI

--- frontend/Team07/src/app/Service/redcorduse.service.ts
+++ frontend/Team07/src/app/Service/redcorduse.service.ts
@@ -11,12 +11,23 @@ export class RedcorduseService {
 
   constructor(private http: HttpClient) { }
 
+  postRedcorduse(symptom: String, drugdataId: Number, staffId: Number, customerId: Number): Observable<any> {
+    console.log(this.API + '/RecordDrugUse-insert/symptom/' + symptom + '/drugdataId/' + drugdataId + '/staffId/' + staffId + '/customerId/' + customerId)
+    return this.http.post(this.API + '/RecordDrugUse-insert/symptom/' + symptom + '/drugdataId/' + drugdataId + '/staffId/' + staffId + '/customerId/' + customerId, {});
 
-  getCustomerById(customerId : number): Observable<any> {
-    return this.http.get(this.API + '/Customer/'+ customerId);
   }
 
-  
+
+
+  getCustomerById(customerId: number): Observable<any> {
+    return this.http.get(this.API + '/Customer/' + customerId);
+  }
+
+  getCustomerByname(customerName: String): Observable<any> {
+    return this.http.get(this.API + '/Customer/customerName/' + customerName);
+  }
+
+
 
 
 }
